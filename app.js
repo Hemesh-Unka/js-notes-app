d7d012110cf00ff91ddf6ad80d60faf975e1721f
Hemesh-Unka <hemeshunka@gmail.com>
2018-06-11 15:09:41 +0100

    completed lecture 13 - capturing user input

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-console.log('Starting app')
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
 
-var user = os.userInfo();
+const notes = require('./notes.js');
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`);
+var command = process.argv[2];
+console.log('Command: ', command);
+console.log(process.argv);
+
+if (command === 'add') {
+    console.log('Adding new log');
+} else if (command === 'list') {
+    console.log('Listing all notes');
+} else if (command === 'read') {
+    console.log('Reading note');
+} else if (command === 'remove') {
+    console.log('Removing note');
+} else {
+    console.log('Command not recognised');
+}
